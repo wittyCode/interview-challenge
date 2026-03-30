f5ebb5c22392bcf1e9e31916d523de91247e37cb
wittycode <michael.wittmann2018@gmail.com>
2026-03-30 16:06:14 +0200

    add styling of table

--- interview-frontend/src/components/CustomerTable.tsx
+++ interview-frontend/src/components/CustomerTable.tsx
@@ -1,4 +1,5 @@
 import type { Customer } from '../types/customer.ts';
+import { MdDelete, MdEdit } from 'react-icons/md';
 
 type CustomerTableProps = {
   customers: Customer[];
@@ -9,30 +10,37 @@ type CustomerTableProps = {
 };
 
 export default function CustomerTable({ customers, editFn, deleteFn, isLoading, errorMsg }: CustomerTableProps) {
-  const customersToRows = customers.map((customer) => (
-    <tr key={customer.id} className="hover:bg-gray-500">
-      <td>{customer.firstName}</td>
+  const rowClasses = 'rounded-2xl bg-white hover:bg-gray-200 border-b-2 border-gray-200';
+  const buttonCellClasses = 'py-4';
+  const lastButtonCellClasses = 'rounded-br-2xl ' + buttonCellClasses;
+  const customersToRows = customers.map((customer, index) => (
+    <tr key={customer.id} className={rowClasses}>
+      <td className={index === customers.length - 1 ? 'rounded-bl-2xl p-4' : 'p-4'}>{customer.firstName}</td>
       <td>{customer.lastName}</td>
       <td>{customer.description}</td>
       <td>{customer.salesTaxId}</td>
       <td>{customer.address}</td>
       <td>{customer.zipCode}</td>
       <td>{customer.city}</td>
-      <td>
-        <button onClick={() => editFn(customer)}>edit</button>
-        <button onClick={() => deleteFn(customer)}>del</button>
+      <td className={index === customers.length - 1 ? lastButtonCellClasses : buttonCellClasses}>
+        <button onClick={() => editFn(customer)} className="hover:cursor-pointer">
+          <MdEdit size="1.5em" />
+        </button>
+        <button onClick={() => deleteFn(customer)} className="hover:cursor-pointer">
+          <MdDelete size="1.5em" />
+        </button>
       </td>
     </tr>
   ));
 
   return (
-    <div>
+    <div className="rounded-2xl border-2 border-gray-200">
       {isLoading && <p>Loading...</p>}
       {(errorMsg && <p>{errorMsg}</p>) || (
         <table className="w-full">
           <thead>
-            <tr>
-              <td>Vorname</td>
+            <tr className="border-gray-200 font-semibold">
+              <td className="p-4">Vorname</td>
               <td>Nachname</td>
               <td>Beschreibung</td>
               <td>Ust-Idnr.</td>
